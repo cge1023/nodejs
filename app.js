3cd4645cccbb442c246b9cd3b65a95d9c59ace7a
cge1023 <ge931023@gmail.com>
2022-08-30 17:44:01 +0900

    ajax axios 서버 통신

--- app.js
+++ app.js
@@ -26,7 +26,36 @@ app.post("/post", (req, res) => {
     gender: req.body.gender,
     birthYear: req.body.birthYear,
   });
-}); //post 요청이 /post라는 주소로 들어왔을 때 안의 함수를 실행
+});
+
+app.post("/get/ajax", (req, res) => {
+  console.log(req.body);
+  var data = {
+    name: req.body.name,
+    gender: req.body.gender,
+  };
+  res.send(data);
+});
+
+app.get("/get/axios", (req, res) => {
+  console.log(req.query);
+  var data = {
+    name: req.query.name,
+    gender: req.query.gender,
+  };
+  res.send(data);
+});
+
+app.post("/post/axios", (req, res) => {
+  console.log(req.body);
+  var data = {
+    name: req.body.name,
+    gender: req.body.gender,
+  };
+  res.send(data);
+});
+
+//post 요청이 /post라는 주소로 들어왔을 때 안의 함수를 실행
 app.listen(port, () => {
   console.log("server open: ", port);
 });
