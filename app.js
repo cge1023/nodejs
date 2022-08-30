6bc4b340445ef6565f2ed8200dbebacdd0cf4010
cge1023 <ge931023@gmail.com>
2022-08-30 15:43:59 +0900

    GET POST 방식

--- app.js
+++ app.js
@@ -2,35 +2,31 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.set("view engine", "ejs");
-
 app.use("/static", express.static("static"));
 
 app.get("/", (req, res) => {
-  let person = [
-    { name: "최고은", gender: "여자" },
-    { name: "홍길동", gender: "남자" },
-  ];
-  res.render("test", { per: person });
+  res.render("main");
 });
 
-app.get("/test1", (req, res) => {
-  let fruits = ["수박", "딸기", "참외", "복숭아"];
-  res.render("test1", { fruit: fruits });
+app.get("/get", (req, res) => {
+  console.log(req.query); //입력된 값 출력
+  res.render("aaaa", {
+    name: req.query.name, //서버에서 받아온 변수 전달
+  });
+  // res.send("get 요청"); //문자열 전송
 });
 
-app.get("/test2", (req, res) => {
-  res.render("test2");
-});
-
-app.get("/watermelon", (req, res) => {
-  res.render("watermelon");
-});
-
-app.get("/purchase", (req, res) => {
-  res.render("purchase");
-});
-
+app.post("/post", (req, res) => {
+  console.log(req.body);
+  res.render("aaaa", {
+    name: req.body.name,
+    gender: req.body.gender,
+    birthYear: req.body.birthYear,
+  });
+}); //post 요청이 /post라는 주소로 들어왔을 때 안의 함수를 실행
 app.listen(port, () => {
   console.log("server open: ", port);
 });
