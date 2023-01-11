d253aa843147f8361d3c2fd8eb2ef8e0128d1543
Kaylyn Choi <101104214+cge1023@users.noreply.github.com>
2023-01-11 13:00:58 +0900

    Delete kdt-2nd directory

--- kdt-2nd/model/Test.js
+++
@@ -1,8 +0,0 @@
-// DB랑 통신하는 곳
-exports.hello = function () {
-  return "hello2";
-};
-
-exports.login = function () {
-  return { id: "cge1023", pw: "1234" };
-};
--- kdt-2nd/model/User.js
+++
@@ -1,5 +0,0 @@
-exports.user = function () {
-  return `spreatics//12341234//코딩온
-codee//4321//codee
-goeun//1234//최고은`;
-};
