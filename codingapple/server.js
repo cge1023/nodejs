35a4af99a52475349ac385b01ed873cbd3db1087
cge1023 <ge931023@gmail.com>
2023-01-16 11:12:56 +0900

    login 기능 완성

--- codingapple/server.js
+++ codingapple/server.js
@@ -5,18 +5,16 @@ app.set("view engine", "ejs");
 const MongoClient = require("mongodb").MongoClient;
 const methodOverride = require("method-override");
 app.use(methodOverride("_method"));
+require("dotenv").config();
 
 let db;
-MongoClient.connect(
-  "mongodb+srv://admin:Xx1023_0901@cluster0.ezfes.mongodb.net/?retryWrites=true&w=majority",
-  (err, client) => {
-    if (err) return console.log(err);
-    db = client.db("todoapp");
-    app.listen(8080, function () {
-      console.log("listening on 8080");
-    });
-  }
-);
+MongoClient.connect(process.env.DB_URL, (err, client) => {
+  if (err) return console.log(err);
+  db = client.db("todoapp");
+  app.listen(process.env.PORT, function () {
+    console.log("listening on 8080");
+  });
+});
 
 app.get("/", (req, res) => {
   res.render("main");
@@ -99,12 +97,14 @@ app.put("/edit", (req, res) => {
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const session = require("express-session");
+const flash = require("express-flash");
 
 app.use(
   session({ secret: "비밀코드", resave: true, saveUninitialized: false })
 );
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(flash());
 
 app.get("/login", (req, res) => {
   res.render("login");
@@ -114,11 +114,28 @@ app.post(
   "/login",
   passport.authenticate("local", {
     failureRedirect: "/fail",
+    failureFlash: true,
   }),
   (req, res) => {
+    console.log(req.body);
     res.redirect("/");
   }
 );
+
+app.get("/mypage", isLoggedin, (req, res) => {
+  console.log("req.user", req.user);
+  res.render("mypage", { user: req.user });
+});
+
+// 마이페이지 접속 전 실행할 미들웨어
+function isLoggedin(req, res, next) {
+  // 로그인 후 세션이 있으면 req.user 가 항상있음
+  if (req.user) {
+    next();
+  } else {
+    res.send("로그인해주세요");
+  }
+}
 
 // 사용자의 아이디와 비번을 검증하는 부분
 passport.use(
@@ -130,20 +147,29 @@ passport.use(
       passReqToCallback: false, // 아이디/비번 말고 다른 정보검사가 필요한지
     },
     function (enteredId, enteredPw, done) {
-      db.collection("login").findOne(
-        { _id: enteredId },
-        function (err, result) {
-          if (err) return done(err);
+      db.collection("login").findOne({ id: enteredId }, function (err, result) {
+        if (err) return done(err);
 
-          if (!result)
-            return done(null, false, { message: "존재하지않는 아이디입니다" });
-          if (enteredPw == result.pw) {
-            return done(null, result);
-          } else {
-            return done(null, false, { message: "비번 틀렸습니다" });
-          }
+        if (!result)
+          return done(null, false, { message: "존재하지않는 아이디입니다" });
+        if (enteredPw == result.pw) {
+          return done(null, result);
+        } else {
+          return done(null, false, { message: "비번 틀렸습니다" });
         }
-      );
+      });
     }
   )
 );
+
+// 세션을 저장시키는 코드 (로그인 성공시 발동)
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+// 이 세션데이터를 가진 사람을 DB에서 찾아주세요 (마이페이지 접속시 발동)
+passport.deserializeUser((id, done) => {
+  // DB에서 위에느는 user.id로 유저를 찾은 뒤에 유저 정보를 넣어줌
+  db.collection("login").findOne({ id: id }, function (err, result) {
+    done(null, result);
+  });
+});
